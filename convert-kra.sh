dcac9f54ed4bf71dfe07bc9b8927d6e36ce101df
moiSentineL <nibir.patowary@gmail.com>
2024-08-24 18:30:19 +0530

    improve kra script

---
+++ convert-kra.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+path="src/pngs"
+base="src/"
+ext=".kra"
+
+if [ -d $path ]
+then
+    for file in src/*.kra ; do
+        suf="${file#$base}" ;
+
+        krita "${file}" --export --export-filename "$path/${suf%$ext}.png" ;
+        done ;
+
+else
+    echo "/src/pngs not found. Creating..."
+    mkdir "src/pngs"
+    for file in src/*.kra ; do
+        suf="${file#$base}" ;
+
+        krita "${file}" --export --export-filename "$path/${suf%$ext}.png" ;
+        done ;
+
+fi
